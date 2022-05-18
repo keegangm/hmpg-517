2a97d018052bb81f1d0802f3ba3f3d7e376c16aa
keegangm <kgmorris76@gmail.com>
2022-05-17 21:29:57 -0500

    Add files via upload

---
+++ script-2.js
@@ -0,0 +1,18 @@
+//Colors array
+let colors = ['#F95738', '#FF5714','#BF4342','#F28482','#FE7F2D','#CE7B91','#BF6900','#5F0A87','#29339B','#446DF6','#9999FF','#BC96E6','#2589BD','#54457F','#C1EDCC','#09814A','#FDE74C','#668F80','#F0A202','#FF9F1C','#6CC551'];
+
+//Get button
+var boxBg = document.querySelector('.color-box');
+
+//add event listener
+addEventListener('click', function(){
+    console.log('test')
+    //randomizer
+    //var randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+    //get container
+    //let cont
+
+    //container style
+    //button.style.background = randomColor;
+})
